bec6477ae965cedb153b69949f8f30f759d3e410
Feurial Black <1713322+FeurialBlack@users.noreply.github.com>
2020-11-30 11:20:22 +0000

    Initial commit

---
+++ 1.2.c
@@ -0,0 +1,13 @@
+// WAP to calculate area of rectangle from std inputs
+#include <stdio.h>
+
+int main(void) {
+	int length,breadth;
+	printf("What is the length of the Rectangle \n");
+	scanf("%d",&length);
+	printf("What is the breadth of the Rectangle \n");
+	scanf("%d",&breadth);
+	printf("Area of a Rectangle is %d",length*breadth);
+	return 0;
+}
+
