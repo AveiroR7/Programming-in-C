bac3269919686ee0a44407aaa4798aeca8237933
Feurial Black <1713322+FeurialBlack@users.noreply.github.com>
2020-11-30 11:40:27 +0000

    Initial commit

---
+++ 1.3.c
@@ -0,0 +1,12 @@
+// WAP to calculate Area of Circle
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+	int radius = 3;
+	float pi =3.14;
+	float Area = pi*radius*radius;
+	printf("Area of Circle is %f",Area);
+	return 0;
+}
+
