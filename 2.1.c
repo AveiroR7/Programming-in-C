bc0bdebd6e2c7fe0b76ce4bb691cb531cd6db878
Feurial Black <1713322+FeurialBlack@users.noreply.github.com>
2020-12-01 02:37:51 +0530

    Initial commit

---
+++ 2.1.c
@@ -0,0 +1,11 @@
+// WAP to determine num divisible by 97 or not?
+#include <stdio.h>
+
+int main(void) {
+    int num;
+    printf("Enter the num \n");
+    scanf("%d",&num);
+    printf("Divisibility test %d \n",num%97);
+	return 0;
+}
+
