794a9bd7534e4b2c8bdc53b5a3d01d8449ce0e01
Feurial Black <1713322+FeurialBlack@users.noreply.github.com>
2020-11-30 12:17:38 +0000

    Initial commit

---
+++ 1.5.c
@@ -0,0 +1,10 @@
+// WAP to convert celcius to farenheit 
+#include <stdio.h>
+
+int main(void) {
+	float celcius=3;
+	float farenheit= (celcius*9/5)+32;
+	printf("The farenheit is %f",farenheit);
+	return 0;
+}
+
