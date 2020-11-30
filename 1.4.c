1254b4aed5608b32a4e9fed693630a789c168af4
Feurial Black <1713322+FeurialBlack@users.noreply.github.com>
2020-11-30 11:46:00 +0000

    Initial commit

---
+++ 1.4.c
@@ -0,0 +1,12 @@
+// WAP to calculate the volume of cylinder
+#include <stdio.h>
+
+int main(void) {
+	int radius=3;
+	int height=4;
+	float pi= 3.14;
+	float volume=pi*radius*radius*height;
+	printf("The volume of the cylinder is %f",volume);
+	return 0;
+}
+
