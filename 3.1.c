4f93333133ffdd268e530c9b470fffc53078ddab
Feurial Black <1713322+FeurialBlack@users.noreply.github.com>
2020-12-02 16:19:05 +0000

    Initial commit

---
+++ 3.1.c
@@ -0,0 +1,22 @@
+/* WAP to findout whether a student is pass or fail, if it requires total 40% and atleast 33 % 
+in each subject to pass assume 3 subjects and take marks as an input from the user.*/
+
+#include <stdio.h>
+
+int main(void) {
+    int physics,chemistry,maths;
+    
+    printf("Enter the Marks of physics :\n");
+    scanf("%d",&physics);
+    
+    printf("Enter the Marks of chemistry :\n");
+    scanf("%d",&chemistry);
+    
+    printf("Enter the Marks of maths :\n");
+    scanf("%d",&maths);
+    
+    
+    
+    return 0;
+}
+
