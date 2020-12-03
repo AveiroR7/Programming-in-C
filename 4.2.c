0a0a84e0e5db8fe8df4e0095fb1b606b39fe2f11
Feurial Black <1713322+FeurialBlack@users.noreply.github.com>
2020-12-03 08:45:43 +0530

    Initial commit

---
+++ 4.2.c
@@ -0,0 +1,18 @@
+// WAP to print Multiplication table of 10 in reverse order.
+#include <stdio.h>
+
+
+int main(void) {
+    int n=10,i;
+    
+    for(i=10;i<=10;i--){
+        printf("%d \n",n*i);
+        if (i == 1){
+            break;
+        }
+        
+    }
+    
+	return 0;
+}
+
