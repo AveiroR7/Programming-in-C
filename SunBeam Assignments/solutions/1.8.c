12062e60e50519d54217c458c30f44fffb0b455a
Feurial Black <1713322+FeurialBlack@users.noreply.github.com>
2020-12-23 10:03:35 +0530

    Create 1.8.c

---
+++ SunBeam Assignments/solutions/1.8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,c,Average;
+	
+	
+	printf("Enter the First Number :\n");
+	scanf("%d",&a);
+	printf("Enter the Second Number :\n");
+	scanf("%d",&b);
+	printf("Enter the Third Number :\n");
+	scanf("%d",&c);
+	Average = (a+b+c)/3;
+	printf("The Average is: %d \n",Average);
+	
+
+	
+	return 0;
+}
+
